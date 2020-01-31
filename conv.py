33c9fe74e496b9534e31a53e0db52e1d146ad4e1
janfki <j.f.kismul@fys.uio.no>
2020-01-31 14:56:17 +0100

    updates

--- conv.py
+++ conv.py
@@ -7,93 +7,69 @@ from dependencies import plotting_convention
 from scipy.optimize import minimize
 from scipy.stats import norm
 import pandas
+import pickle
 
-
-#create normally distributed firing rates per kernel
-rv = norm(loc=25, scale=8)
-rv1 = norm(loc=60,scale=10)
-rv2 = norm(loc=80,scale=10)
-rvs=[rv,rv1,rv2]
+kerns = pickle.load(open('data/kernels.p','rb'))
+dt_k = pickle.load(open('data/dts.p','rb'))
+kerns_to_run=[0,1]
+# kerns_to_run = [0,1,3,4]
+# kerns_to_run = [0,1,2,3,4,5]
+param_idx = []
+for i in kerns_to_run:
+    param_idx.append(i*3)
+    param_idx.append(i * 3+1)
+    param_idx.append(i * 3+2)
 
 #3read real data
 df_on = pandas.read_csv('data/inhib_on.csv')
 df_off = pandas.read_csv('data/inhib_blocked.csv')
 
 # parameters
-num_tsteps = 1000
-# num_tsteps = len(df_on['x'])
-kernel_num_tsteps = 200
-num_kernels = 3
-dt = 0.1
+num_tsteps = len(df_off['x'])
+kernel_num_tsteps = len(kerns[0])
+num_kernels = len(kerns[1])
+dt = 20./num_tsteps
+
+dt_k = dt_k/kernel_num_tsteps
 t = np.arange(num_tsteps) * dt
 
-taus = [0.5, 2, 3] #timeconstants
-amps = [1, -0.7, -0.25] #amplitudes
 colors = ['b', 'r', 'orange']
 colors_opt = ['cyan', 'pink', 'yellow']
 kernels = []
-for idx in range(num_kernels):
-    k_ = np.zeros(kernel_num_tsteps)
-    k_[int(kernel_num_tsteps / 2):] = amps[idx] * np.exp(-t[:int(kernel_num_tsteps/2)] / taus[idx])
-    kernels.append(k_)
 
-t_locs = [25, 60, 80]
-stds = [8, 10, 10]
-amps = [1.8, 1, 0.7]
+for idx in kerns_to_run:
+    k_ = kerns[1][idx]
+    kernels.append(k_)
+num_kernels_to_run = len(kernels)
 
 num_params_per_kernel = 3
 
-correct = np.zeros(num_kernels*num_params_per_kernel)
-for j,i in enumerate(range(0,num_kernels*num_params_per_kernel,3)):
-    correct[i]= t_locs[j]
-    correct[i+1]=stds[j]
-    correct[i+2]=amps[j]
-
-firing_rates = [amps[idx] * rvs[idx].pdf(t)
-                for idx in range(num_kernels)]
-
-data = np.zeros(num_tsteps)
-for idx in range(num_kernels):
-    data += ss.convolve(firing_rates[idx], kernels[idx], mode="same")
-
-x0 = [50, 10, 1,
-      50, 10, 1,
-      50, 10, 1,
-      ]
+data = np.asarray(df_off['inhib_blocked'])
 ## VELDIG AVHENGIG AV IINiTALBETINGELSER
 ## LITT TESTING KAN TYDE PÅ AT HØYE VERDIER TREFFER BEDRE ENN LAVE
-x0 = [50, 10, 1,
-      90, 10, 1,
-      90, 10, 1,
+x0 = [15, 5, 1,
+      15, 5, 1,
+      15, 5, 1,
+      15, 5, 1,
+      15, 5, 1,
+      15, 5, 1
       ]
 
-# bounds = [
-#     [t[0], t[-1]], [0, 50], [0, 2],
-#     [t[0], t[-1]], [0, 50], [0, 2],
-#     [t[0], t[-1]], [0, 50], [0, 2],
-#     ]
 bounds = [
     #   loc           std       amp
-    [t[0], t[-1]], [1e-7, 50], [0, 2],
-    [t[0], t[-1]], [1e-7, 50], [0, 2],
-    [t[0], t[-1]], [1e-7, 50], [0, 2],
+    [t[0], t[-1]], [1e-7, 10], [0, 5],
+    [t[0], t[-1]], [1e-7, 10], [0, 5],
+    [t[0], t[-1]], [1e-7, 10], [0, 5],
+    [t[0], t[-1]], [1e-7, 10], [0, 5],
+    [t[0], t[-1]], [1e-7, 10], [0, 5],
+    [t[0], t[-1]], [1e-7, 10], [0, 5]
     ]
-# print(x0)
-# print(bounds)
-def blah(data,l1,l2,l3,s1,s2,s3,a1,a2,a3):
-    RV1 = norm(loc=l1,scale=s1)
-    RV2 = norm(loc=l2, scale=s2)
-    RV3 = norm(loc=l3, scale=s3)
-    fits = ss.convolve(a1*RV1.pdf(t),kernels[0],mode='same')+ss.convolve(a2*RV2.pdf(t),kernels[1],mode='same')+ ss.convolve(a3*RV3.pdf(t),kernels[2],mode='same')
-    return np.sum((data-fits)**2)
 
 def minimize_firing_rates(x, *args):
     data = args[0]
-    fit_ = np.zeros(num_tsteps)
-    for idx in range(num_kernels):
+    fit_ = np.zeros(len(data[0]))
+    for idx in range(num_kernels_to_run):
         RV = norm(loc=x[0 + idx * num_params_per_kernel], scale=x[1 + idx * num_params_per_kernel])
-        # if scale = 0, the above whines. Adding a small number to scale
-        # RV = norm(loc=x[0 + idx * num_params_per_kernel], scale=x[1 + idx * num_params_per_kernel]+1e-7)
         amp_ = x[2 + idx * num_params_per_kernel]
         firing_rate_ = amp_ * RV.pdf(t)
         fit_ += ss.convolve(firing_rate_, kernels[idx],mode="same")
@@ -102,16 +78,14 @@ def minimize_firing_rates(x, *args):
 
 args = [data]
 res = minimize(minimize_firing_rates, x0, args=args, bounds=bounds,tol=1e-25,options={'eps':1e-4})
-print('predicted {}'.format(res['x'].round(1)))
-print('correct  ',correct.round(1))
+print('predicted {}'.format(res['x'][param_idx].round(1)))
+print('initial  ',np.asarray(x0)[param_idx])
 print('error pred',minimize_firing_rates(res['x'],args))
-print('error correct',minimize_firing_rates(correct,args))
 print(res['message'])
 
-# print(res)
 fit = np.zeros(num_tsteps)
 firing_rates_opt = []
-for idx in range(num_kernels):
+for idx in range(num_kernels_to_run):
     RV = norm(loc=res.x[0 + idx * num_params_per_kernel],scale=res.x[1 + idx * num_params_per_kernel])
     amp_ = res.x[2 + idx * num_params_per_kernel]
     firing_rate_ = amp_ * RV.pdf(t)
@@ -126,12 +100,10 @@ ax_sig = fig.add_subplot(133, title="signal (convolution)", xlabel="time")
 
 lines = []
 line_names = []
-for idx in range(num_kernels):
-    l, = ax_fr.plot(t, firing_rates[idx], c=colors[idx])
-    l_, = ax_fr.plot(t, firing_rates_opt[idx], c=colors_opt[idx], ls='--')
-    ax_k.plot(np.arange(kernel_num_tsteps) * dt, kernels[idx][:kernel_num_tsteps], c=colors[idx])
-    lines.append(l)
-    line_names.append("Kernel {}".format(idx))
+for idx in range(num_kernels_to_run):
+    l_, = ax_fr.plot(t, firing_rates_opt[idx])#, c=colors_opt[idx], ls='--')
+    ax_k.plot(np.arange(kernel_num_tsteps) * dt_k,kernels[idx])#, c=colors[idx])
+
     lines.append(l_)
     line_names.append("firing rate fit {}".format(idx))
 
--- kernels.py
+++ kernels.py
@@ -1,13 +1,7 @@
 import LFPy
 import numpy as np
 import matplotlib.pyplot as plt
-import sys
-
-
-if len(sys.argv)>1:
-    iters = int(sys.argv[1])
-else:
-    iters = 1
+import pickle
 
 morphology = 'morphologies/L5_Mainen96_LFPy.hoc'
 
@@ -30,7 +24,7 @@ def insert_synapses(synparams, section, n):
 
         # Create synapse(s) and setting times using the Synapse class in LFPy
         s = LFPy.Synapse(cell, **synparams)
-        s.set_spike_times(np.array([20.]))
+        s.set_spike_times(np.array([cell.tstop/2.]))
 
 
 cell_parameters = {
@@ -38,39 +32,32 @@ cell_parameters = {
     'cm': 1.0,  # membrane capacitance
     'Ra': 150.,  # axial resistance
     'v_init': -65.,  # initial crossmembrane potential
-    'passive': True,  # turn on NEURONs passive mechanism for all sections
-    # 'passive': False,  # turn on NEURONs passive mechanism for all sections
-
+    # 'passive': True,  # turn on NEURONs passive mechanism for all sections
+    'passive': False,  # turn on NEURONs passive mechanism for all sections
     'passive_parameters': {'g_pas': 1. / 30000, 'e_pas': -65},
     'nsegs_method': 'lambda_f',  # spatial discretization method
     'lambda_f': 100.,  # frequency where length constants are computed
     'dt': 2. ** -3,  # simulation time step size
     'tstart': -300.,  # start time of simulation, recorders start at t=0
-    'tstop': 100.,  # stop simulation at 100 ms.
+    'tstop': 50.,  # stop simulation at 100 ms.
 }
 # Define synapse parameters
 synapse_parameters = {
     'idx':[],
     'e': 0,  # reversal potential
     # 'syntype': 'ExpSyn',  # synapse type
-    # 'tau': 1.,  # synaptic time constant
+    # 'tau': 0.005,  # synaptic time constant
     'syntype': 'Exp2Syn',
-    'tau1':.2,
-    'tau2':1.,
-
+    'tau1':1.,
+    'tau2':3.,
     'weight': .001,  # synaptic weight
     'record_current': True,  # record synapse current
 }
 
-# soma_pos = np.array([0.,0.,0.])
 p = []
 print("running simulation...")
-
 n_syn=200
 np.random.seed(10)
-# for j in range(iters):
-#     print('iteration', j+1, 'of', iters)
-#     np.random.seed(j+10)
 for i in range(6):
     # Create cell
     cell = LFPy.Cell(**cell_parameters)
@@ -81,30 +68,22 @@ for i in range(6):
     else:
         synapse_parameters['e'] = 0
     if i in [0, 3]:
-        # np.random.seed(1234+j)
         np.random.seed(1234)
         insert_synapses(synapse_parameters,'dend',n_syn)
     if i in [1,4]:
-        # np.random.seed(1234+j)
         np.random.seed(1234)
         insert_synapses(synapse_parameters,'apic',n_syn)
     else:
         np.random.seed(1234)
         insert_synapses(synapse_parameters,'allsec',n_syn)
 
-
-
-    synapse = LFPy.Synapse(cell, **synapse_parameters)
-    synapse.set_spike_times(np.array([20.]))
-    # synapse.set_spike_times(np.array([10.]))
-
     cell.simulate(rec_imem=True,rec_vmem=True,rec_current_dipole_moment=True)
 
     P = cell.current_dipole_moment
     p.append(P)
-
 print("done")
 
+#createfigure
 plt.figure()
 p = np.asarray(p)/n_syn
 legs = ['ed','ea','ef','id','ia','if']
@@ -113,6 +92,5 @@ for i in range(6):
 plt.legend()
 plt.savefig('plots/kernels.png')
 
-# plt.plot()
-# plt.plot(cell.tvec,cell.vmem[0])
-# plt.show()
+pickle.dump([cell.tvec,p[:,:,2]],open('data/kernels.p','wb'))
+pickle.dump(cell_parameters['tstop'],open('data/dts.p','wb'))
